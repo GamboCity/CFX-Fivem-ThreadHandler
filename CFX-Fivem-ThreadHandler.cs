6ad3c540086ce7030ff85edce1519f16e9ff9659
Keyinator <k3yinator@gmail.com>
2022-11-15 14:35:56 +0100

    Update
    Update

--- CFX-Fivem-ThreadHandler.cs
+++ CFX-Fivem-ThreadHandler.cs
@@ -1,26 +1,69 @@
 namespace CFX-Fivem-ThreadHandler {
-    public static class ThreadHandler {
+    public sealed class ThreadHandler {
         public static bool IsMainThread => Thread.CurrentThread.ManagedThreadId == 3;
 
-        public static async Task GoToMainThread() {
-            if (!IsMainThread) await BaseScript.Delay(0);
+        public static async Task ConditionalJumpToMainThread() {
+            if (!IsMainThread)
+                await BaseScript.Delay(0);
         }
 
-        public static async Task LeaveMainThread() {
-            if (IsMainThread) await Task.Run(() => { }).ConfigureAwait(false);
+        public static void ThrowIfNotMainThread() {
+            if (!IsMainThread) throw new Exception("!!! NOT ON MAIN THREAD");
         }
 
-        public static async Task CallOnMainThread(Action functionToInvoke) {
-            await GoToMainThread();
-            functionToInvoke();
-            await LeaveMainThread();
+        public static async Task CallOnMainThread(string name, Action functionToInvoke) {
+            if (IsMainThread) {
+                functionToInvoke();
+                return;
+            }
+
+            await Task.Run(async () => {
+                await BaseScript.Delay(0);
+                ThrowIfNotMainThread();
+                functionToInvoke();
+                ThrowIfNotMainThread();
+            })
+            .ContinueWith(t => {
+                if (t.IsFaulted)
+                    throw t.Exception;
+            }).ConfigureAwait(false);
         }
 
-        public static async Task<T> CallOnMainThread<T>(Func<T> functionToInvoke) {
-            await GoToMainThread();
-            T _result = functionToInvoke();
-            await LeaveMainThread();
-            return _result;
+        public static async Task CallOnMainThread(string name, Func<Task> functionToInvoke) {
+            if (IsMainThread) {
+                await functionToInvoke().ConfigureAwait(false);
+                return;
+            }
+
+            await Task.Run(async () => {
+                await BaseScript.Delay(0);
+                ThrowIfNotMainThread();
+                await functionToInvoke().ConfigureAwait(false);
+                ThrowIfNotMainThread();
+            })
+            .ContinueWith(t => {
+                if (t.IsFaulted)
+                    throw t.Exception;
+            }).ConfigureAwait(false);
+        }
+
+        public static async Task<T> CallOnMainThread<T>(string name, Func<T> functionToInvoke) {
+            if (IsMainThread) {
+                return functionToInvoke();
+            }
+
+            return await Task.Run(async () => {
+                await BaseScript.Delay(0);
+                ThrowIfNotMainThread();
+                var temp = functionToInvoke();
+                ThrowIfNotMainThread();
+                return temp;
+            })
+            .ContinueWith(t => {
+                if (t.IsFaulted)
+                    throw t.Exception;
+                return t.Result;
+            }).ConfigureAwait(false);
         }
     }
 }
